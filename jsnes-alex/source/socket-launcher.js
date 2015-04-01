679d2bdcd1b8d3912ed738378d1e05288db16aa6
Alex Halderman <jhalderm@eecs.umich.edu>
2015-04-01 00:12:29 -0400

    Embetter

--- jsnes-alex/source/socket-launcher.js
+++ jsnes-alex/source/socket-launcher.js
@@ -2,15 +2,14 @@ var wsUri = 'ws://localhost:9000/';
 
 window.addEventListener("load", function() {
 
-    // init websocket
-    var websocket = new WebSocket(wsUri);    
-    websocket.binaryType = "arraybuffer";
+    // kindle a websocket
+    var websocket = new WebSocket(wsUri);
     function status(m) { document.getElementById('socketStatus').innerHTML = m; }
     websocket.onopen  = function(e) { status('CONNECTED'); };
     websocket.onclose = function(e) { status('DISCONNECTED'); };
     websocket.onerror = function(e) { status('ERROR: '+e.data); };
 
-    // on every frame, send a png of the canvas to the websocket
+    // on every frame, send a png of the canvas over the socket
     var origFrame = JSNES.prototype.frame;
     JSNES.prototype.frame = function() {
         websocket.send(document.querySelector('.nes-screen').toDataURL("image/png"));
