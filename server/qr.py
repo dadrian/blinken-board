d680d0f38e46a84f9784a9902c484d5df91fdcd2
Eric Wustrow <ewust@umich.edu>
2015-09-22 02:39:44 -0400

    add waitframes to qr code

--- server/qr.py
+++ server/qr.py
@@ -3,9 +3,10 @@ import qrcode
 import random
 
 class QR(object):
-    def __init__(self, board, url):
+    def __init__(self, board, url, wait_frames=50*30):
         self.board = board
         self.url = url
+        self.wait_frames = wait_frames  # number of frames to keep the animation at at the end
 
         qr = qrcode.QRCode(
             version=6,
@@ -43,15 +44,19 @@ class QR(object):
                 yield  # send board, sleep a bit...repeat
 
         # maintain this board for a bit
-        for x in range(50*30):
+        for x in range(self.wait_frames):
             yield
 
 
 if __name__ == '__main__':
     from board import Board
     import time
+    import sys
     board = Board(use_pygame=True)
-    qr = QR(board, 'http://wallhacks.xyz/#VfcgOCskLOcq')
+    url = 'http://wallhacks.xyz/#VfcgOCskL0cq'
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    qr = QR(board, url)
     for f in qr.frames():
         board.display()
         time.sleep(0.015)
