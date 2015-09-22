d19a021222526286e034ad10cd99efb1c2217063
Eric Wustrow <ewust@umich.edu>
2015-09-22 02:41:16 -0400

    add stand alone idle frame

---
+++ server/idle_frame.py
@@ -0,0 +1,52 @@
+
+
+import struct
+import sun
+import time
+import logger
+from qr import QR
+from qrgol import QRtoGOL
+import hmac
+import base64
+from board import Board
+
+
+TOKEN_HMAC_KEY = b'fogBI6ymJDbQCf6KVVr5x14r'
+TOKEN_HMAC_TAG_LEN = 5   # bytes
+QR_CODE_PATH = 'http://127.0.0.1:8001/index.html'
+
+def main():
+    board = Board(use_pygame=False, create_ws=True)
+    last_civil = True
+    while True:
+        if (time.gmtime().tm_sec == 0) and time.gmtime().tm_min % 5 == 0:
+            if sun.is_civil_twilight():
+                # keep lights off
+                if last_civil is False:
+                    logger.info('Ending QR display (start of civil twilight)')
+                last_civil = True
+            else:
+                if last_civil is True:
+                    logger.info('Starting QR display (end of civil twilight)')
+                last_civil = False
+                logger.debug('New QR code animation...')
+
+                tb = struct.pack('>L', int(time.time()))            # get time as 4-byte array
+                tag = hmac.new(TOKEN_HMAC_KEY, tb).digest()[0:TOKEN_HMAC_TAG_LEN]    # truncated hmac with secret
+                token = base64.urlsafe_b64encode(tb + tag)
+                url = QR_CODE_PATH + '#' + str(token, 'ascii')
+
+                logger.debug('url: %s' % (url))
+
+                qr = QRtoGOL(board, url, qr_wait_frames=30*3)
+
+                for f in qr.frames():
+                    board.send_board_ws()
+                    time.sleep(1/20.0)
+        time.sleep(0.5)
+
+
+
+if __name__=='__main__':
+    logger.setLogLevel(logger.TRACE)
+    main()
