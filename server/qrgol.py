31071043999d2aa9f9e548d3334aed8048c14397
Eric Wustrow <ewust@umich.edu>
2015-09-22 02:43:35 -0400

    add qr stapled to game of life

---
+++ server/qrgol.py
@@ -0,0 +1,29 @@
+
+
+from qr import QR
+from gameoflife import GameOfLife
+from board import Board
+
+# shows the QR code animation, then turns it into a game of life animation
+class QRtoGOL(object):
+    def __init__(self, board, url, qr_wait_frames=None):
+        self.qr = QR(board, url)
+        if qr_wait_frames is not None:
+            self.qr.wait_frames = qr_wait_frames
+        self.game = GameOfLife(board, self.qr.matrix)
+
+    def frames(self):
+        yield from self.qr.frames()
+        # intermediate? better transition?
+        yield from self.game.frames()
+
+
+if __name__ == '__main__':
+    import time
+    board = Board()
+    q = QRtoGOL(board, "http://wallhacks.xyz/#Vf4aaeja8", qr_wait_frames=30*5)
+
+    for f in q.frames():
+        board.display()
+        time.sleep(1/30.0)
+
