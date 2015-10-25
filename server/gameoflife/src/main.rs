d18bd2cd5aa87bdaf05eef7a10e7fa696360f096
Eric Wustrow <ewust@umich.edu>
2015-10-24 22:54:23 -0400

    rust of gameoflife, import works

---
+++ server/gameoflife/src/main.rs
@@ -0,0 +1,60 @@
+
+//use serialize::base64::{self, ToBase64};
+
+
+
+extern crate rustc_serialize;
+//use rustc_serialize::base64;
+use rustc_serialize::base64::FromBase64;
+
+use std::env;
+
+
+// TODO: take these as command line args
+const WIDTH : usize = 57;
+const HEIGHT : usize = 44;
+
+fn print_board(board :&[[bool; HEIGHT]]) {
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            if board[x][y] {
+                print!(".");
+            } else {
+                print!(" ");
+            }
+        }
+        print!("\n");
+    }
+}
+
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please provide initial board state");
+        std::process::exit(0);
+    }
+
+    let initial_board = &args[1];    // Can't "take" args[1], it is owned
+
+    let bin_board = initial_board.from_base64().unwrap();
+    //assert_eq!(initial_board_res.is_ok(), true);
+    //let bin_board = initial_board_res.ok();
+
+    let mut board = [[false; HEIGHT]; WIDTH];
+
+    println!("Initial board: {}", initial_board);
+    println!("Bin boad: {:?} {}", bin_board, bin_board.len());
+
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            let idx = (x*HEIGHT) + y;
+            let px = bin_board[(idx / 8)] & (1 << (idx % 8));
+            println!("{},{} = {}", x, y, px);
+            board[x][y] = (px != 0);
+        }
+    }
+    print_board(&board);
+}
